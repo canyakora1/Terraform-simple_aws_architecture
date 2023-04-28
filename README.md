de35b1e91231bb107e22cc47188809471457b219
dcyberguy <anyakorachinedu@yahoo.com>
2023-04-27 20:58:50 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Terraform-simple_aws_architecture
 Deploying a simple AWS architecture using Terraform
+which includes;
+
+1. A VPC
+2. Two Subnets (One Private and One Public)
+3. Routle table for each subnets
+4. Security Groups for all instances (EC2) deployed in each subnet
+5. One Internet gateway
+6. One ec2 instance in the public subnet and two instances in the private subnet
+
+#####################################################################################
+#               public (DMZ) Subnet                                                 #
+#       CIDR:           10.0.1.0/24                                                 #
+#       Route Table:    Associated with the Public route table                      #
+#       Instance:       One ec2 instance for the publically accessible webserver    # 
+#       Security Group: Allow only http traffic                                     #
+#                                                                                   #
+#####################################################################################
+
+
+#####################################################################################
+#               private (App) Subnet                                                #
+#       CIDR:           10.0.2.0/24                                                 #
+#       Route Table:    Associated with the private route table                     #
+#       Instance:       One ec2 instance each for both App and DB servers           # 
+#       Security Group: Allow only traffic via web SG                               #
+#                                                                                   #
+#####################################################################################
+
+
+#####################################################################################
+#               private (DB) Subnet                                                 #
+#       CIDR:           10.0.3.0/24                                                 #
+#       Route Table:    Associated with the private route table                     #
+#       Instance:       One ec2 instance for DB servers                             # 
+#       Security Group: Allow only traffic from app subnet                          #
+#                                                                                   #
+#####################################################################################
---
+++ main.tf
@@ -0,0 +1,173 @@
+# Create a VPC
+resource "aws_vpc" "kids_castle" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create public and private subnets
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "private_app_subnet" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_subnet" "private_db_subnet" {
+    vpc_id = aws_vpc.kids_castle.id
+    cidr_block = "10.0.3.0/24"
+  
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "castle_igw" {
+  vpc_id = aws_vpc.kids_castle.id
+}
+
+# Create a NAT gateway and ELP aalocation
+resource "aws_eip" "nat_eip" {
+    vpc = true
+    tags = {
+      Name = "nat_eip"
+    }
+}
+resource "aws_nat_gateway" "private_nat_gw" {
+    connectivity_type = "public"
+    subnet_id = aws_subnet.public_subnet.id
+    allocation_id = aws_eip.nat_eip.id
+    
+    tags = {
+        Name = "private_nat_gw"
+  }
+}
+
+# Create a route table and associate it with the public subnet
+resource "aws_route_table" "public_rt" {
+
+  vpc_id = aws_vpc.kids_castle.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.castle_igw.id
+  }
+  tags = {
+    Name = "public_rt"
+  }
+}
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+# Create a private route table and associate it with the private subnets
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.kids_castle.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_nat_gw.id
+  }
+}
+resource "aws_route_table_association" "private_rt_association" {
+    subnet_id = aws_subnet.private_app_subnet.id
+    route_table_id = aws_route_table.private_rt.id
+  
+}
+# Create security groups
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-"
+
+  ingress {
+    from_port   = 80
+    description = "Allow http web traffic"
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web_sg"
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name_prefix = "app-"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  tags = {
+    Name = "app_sg"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name_prefix = "db-"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.app_sg.id]
+  }
+
+  tags = {
+    Name = "db_sg"
+  }
+}
+
+# Launch EC2 instances
+resource "aws_instance" "web-server" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  user_data = <<EOF
+              #!/bin/bash
+              echo "Hello from web instance" > index.html
+              nohup python -m SimpleHTTPServer 80 &
+              EOF
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_app_subnet.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  user_data = <<EOF
+              #!/bin/bash
+              echo "Hello from app instance" > index.html
+              nohup python -m SimpleHTTPServer 8080 &
+              EOF
+
+  tags = {
+    Name = "app_server"
+  }
+}
+
+resource "aws_instance" "db_server" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_db_subnet.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  user_data = <<EOF
+              #!/bin/bash
+              echo "Hello from app instance" > index.html
+              nohup python -m SimpleHTTPServer 8080 &
+              EOF
+
+  tags = {
+    Name = "db_server"
+  }
+
+}
